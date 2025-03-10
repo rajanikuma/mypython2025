7c9d2ee0b575b16ef3d160322598837c101df40d
rajanikuma <rajani789988@gmail.com>
2025-03-10 16:11:37 +0530

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,8 @@
+from pickle import ADDITEMS
+#program arithmetic operation
+a= int(input("enter a"))
+b=int(input("enter b"))
+print("addition a+b")
+print("subtraction a-b")
+print("multiplication a*b")
+print("division a/b")
