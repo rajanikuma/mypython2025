f3a9cb24fb685feb0bf4f6ac794ec83dace84f5a
rajanikuma <rajani789988@gmail.com>
2025-03-12 18:43:58 +0530

    Create Sum of square of first N numbers.py

---
+++ Sum of square of first N numbers.py
@@ -0,0 +1,8 @@
+
+
+def sum_of_squares(N):
+    return (N * (N + 1) * (2 * N + 1)) // 6
+
+N = int(input("Enter a positive integer N: "))
+print("Sum of squares of first", N, "natural numbers is:", sum_of_squares(N))
+print (" Rajani 60")
