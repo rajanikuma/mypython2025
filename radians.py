0d9b17f303d0012e93108ebf0e5c14c25d25ec78
rajanikuma <rajani789988@gmail.com>
2025-03-12 12:53:18 +0530

    Create radians.py

---
+++ radians.py
@@ -0,0 +1,6 @@
+import math
+angle = float(input("Enter an angle in degreesL"))
+radian = math.radians(angle)
+print("sine:",math.sin(radian))
+print("cosine:",math.cos(radian))
+print("tangent:",math.tan(radian))
