99fd7c12acc9986f9cc56b551ef797946fdede78
rajanikuma <rajani789988@gmail.com>
2025-03-12 17:47:53 +0530

    Create Filter and map function.py

---
+++ Filter and map function.py
@@ -0,0 +1,9 @@
+
+numbers = [10,9,8,7,6,5,4,3,2,1]
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+
+squared_numbers = list(map(lambda x: x ** 2, even_numbers))
+
+print("Even numbers:", even_numbers)
+print("Squared even numbers:", squared_numbers)
+print (" Rajani 60")
