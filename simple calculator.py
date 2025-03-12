6ac8078963f0700e9f2dea9a1f370cebf9013e9b
rajanikuma <rajani789988@gmail.com>
2025-03-12 15:29:17 +0530

    Create simple calculator.py

---
+++ simple calculator.py
@@ -0,0 +1,18 @@
+def calculate(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero is not allowed."
+    else:
+        return "Error: Invalid operator."
+
+
+result = calculate(10,5 ,'+')
+print(result)  
