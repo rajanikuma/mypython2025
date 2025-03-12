8f44e58741ffaec69d8b54b29bdefbe348616c4f
rajanikuma <rajani789988@gmail.com>
2025-03-12 18:27:32 +0530

    Create Break and continue statements.py

---
+++ Break and continue statements.py
@@ -0,0 +1,15 @@
+
+for num in range(1, 10):  
+    if num == 5:  
+        print("Breaking the loop at", num)  
+        break  
+    print(num)
+
+for num in range(1, 10):  
+    if num == 5:  
+        print("Skipping", num)  
+        continue  
+    print(num)
+
+
+
