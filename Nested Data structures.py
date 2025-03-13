9168ac71abcc802edb420967d7ba9b6b853c11cf
rajanikuma <rajani789988@gmail.com>
2025-03-13 17:43:25 +0530

    Create Nested Data structures.py

---
+++ Nested Data structures.py
@@ -0,0 +1,11 @@
+
+print (" rajani 60")
+students = {
+    "Alice": [85, 90, 78],
+    "Bob": [76, 88, 95],
+    "Charlie": [92, 87, 85]
+}
+
+print("Student Marks Dictionary:")
+for name, marks in students.items():
+    print(f"{name}: {marks}")
