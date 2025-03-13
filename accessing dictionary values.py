9f4b116587ae056280cd6fdbfb9b0c49f1b25b4c
rajanikuma <rajani789988@gmail.com>
2025-03-13 15:35:19 +0530

    Create accessing dictionary values.py

---
+++ accessing dictionary values.py
@@ -0,0 +1,11 @@
+my_dict = {
+    "name": "Alice",
+    "age": 25,
+    "city": "New York"
+}
+name_value = my_dict["name"]  
+age_value = my_dict["age"]  
+city_value = my_dict["city"]  
+print("Name:", name_value)
+print("Age:", age_value)
+print("City:", city_value)
