9f2e80dfeed942972a58493a94950e16773ddbeb
rajanikuma <rajani789988@gmail.com>
2025-03-13 16:09:40 +0530

    Create convert list to set and removing duplicates.py

---
+++ convert list to set and removing duplicates.py
@@ -0,0 +1,6 @@
+my_list = [1, 2, 3, 4, 5, 3, 2, 6, 7, 8, 5]
+my_set = set(my_list)
+unique_list = list(my_set)
+print("List with duplicates:", my_list)
+print("List after removing duplicates:", unique_list)
+print("rajani")
