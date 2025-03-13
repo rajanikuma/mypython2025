ec7b42ae8abae4fafb823781a150ef7744aca670
rajanikuma <rajani789988@gmail.com>
2025-03-13 15:49:20 +0530

    Create check key in a dictionary.py

---
+++ check key in a dictionary.py
@@ -0,0 +1,12 @@
+my_dict = {
+    "name": "Alice",
+    "age": 25,
+    "city": "New York"
+}
+key_to_check = "age"
+
+if key_to_check in my_dict:
+    print(f"The key '{key_to_check}' exists in the dictionary.")
+else:
+    print(f"The key '{key_to_check}' does not exist in the dictionary.")
+print("rajani")
