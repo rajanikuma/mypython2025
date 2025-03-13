69372e9ade314bbfccf4a27c0fcb90880ca46d52
rajanikuma <rajani789988@gmail.com>
2025-03-13 14:27:48 +0530

    Create list indexing.py

---
+++ list indexing.py
@@ -0,0 +1,10 @@
+my_list = [10, 20, 30, 40, 50]
+print("rajani 60")
+
+first = my_list[0]
+middle = my_list[len(my_list) // 2]
+last = my_list[-1]
+
+print("First:", first)
+print("Middle:", middle)
+print("Last:", last)
