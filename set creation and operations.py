4d90c60b34d99b265dfaff684f836ca103a483c6
rajanikuma <rajani789988@gmail.com>
2025-03-13 15:52:16 +0530

    Create set creation and operations.py

---
+++ set creation and operations.py
@@ -0,0 +1,9 @@
+print("rajani")
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+union_result = set1 | set2  
+intersection_result = set1 & set2  
+difference_result = set1 - set2  
+print("Union of sets:", union_result)
+print("Intersection of sets:", intersection_result)
+print("Difference of sets (set1 - set2):", difference_result)
