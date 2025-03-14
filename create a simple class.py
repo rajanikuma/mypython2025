f1e5b67cd86defab13b95ab08163cf69cd0638f4
rajanikuma <rajani789988@gmail.com>
2025-03-14 15:13:53 +0530

    Create create a simple class.py

---
+++ create a simple class.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def display_info(self):
+        print(f"Brand: {self.brand}")
+        print(f"Model: {self.model}")
+my_car = Car("Toyota", "Corolla")
+my_car.display_info()
