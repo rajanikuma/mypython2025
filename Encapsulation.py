2da4d1ee33995e10c199331d78556262043bf3a4
rajanikuma <rajani789988@gmail.com>
2025-03-14 10:56:34 +0530

    Create Encapsulation.py

---
+++ Encapsulation.py
@@ -0,0 +1,10 @@
+from typing_extensions import Self
+class  rajani:
+   def __init__(self):
+     self.public = "ok"
+     self.__rajesh = "not ok"
+   def lakshmi_private(self):
+    print(self.__rajesh)
+neha=rajani()
+print(neha.public)
+neha.lakshmi_private()
